3f64c6c2f01cac50a34cd742a914975fa2b8dfbb
Ivan Porta <porta.ivan@outlook.com>
2021-10-13 07:50:25 +0000

    add rss fetch

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,6 @@
-https://gtrekter.medium.com/feed
+const RSS_URL = 'https://gtrekter.medium.com/feed';
+
+fetch(RSS_URL)
+  .then(response => response.text())
+  .then(str => new window.DOMParser().parseFromString(str, "text/xml"))
+  .then(data => console.log(data))
