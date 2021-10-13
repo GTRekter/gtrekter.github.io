ea01af6717fb068d0c78c7d3bb2f856bc0a0fbb8
Ivan Porta <ivanporta@microsoft.com>
2021-10-13 10:13:38 +0200

    add cloud

--- assets/js/script.js
+++
@@ -1,6 +0,0 @@
-const RSS_URL = 'https://gtrekter.medium.com/feed';
-
-fetch(RSS_URL)
-  .then(response => response.text())
-  .then(str => new window.DOMParser().parseFromString(str, "text/xml"))
-  .then(data => console.log(data))
